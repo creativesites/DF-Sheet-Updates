0bc5f494f58fbaabed21aaf650622bca1f595b26
Winston <46081328+creativesites@users.noreply.github.com>
2022-11-01 05:40:03 -0700

    create agent added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { Ints } = require('./ints');
 const { Ents } = require('./ents');
+const { Create } = require('./createAgent');
 
 router.route("/updateInts").post(Ints);
 router.route("/updateEnts").post(Ents);
+router.route("/createAgent").post(Create);
 module.exports = router
